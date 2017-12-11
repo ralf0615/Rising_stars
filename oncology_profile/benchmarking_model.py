2e575d46d5e5de60f2200b6d1e2973455b65b1e7
Yuchen Li <liyuchen0615@hotmail.com>
2017-12-11 16:05:31 -0500

    Benchmarking_model_error_fixed
    age_cutoff does not work properly

--- oncology_profile/benchmarking_model.py
+++ oncology_profile/benchmarking_model.py
@@ -10,26 +10,27 @@ Created on Mon Dec 11 14:34:07 2017
 
 '''
 Benchmarking Rising Stars according to "Non_KOL_with_benchmark_1_15_transpose.csv"
-Benchmark 1-15:
-            2.633333333	
-            3.4	
-            4.4	
-            5.766666667	
-            7.5	
-            9.333333333	
-            10.93333333	
-            12.33333333	
-            14.33333333	
-            17.6	
-            20.03333333	
-            22.96666667	
-            26.96666667	
-            31.53333333	
-            37.6
+Benchmark 1-15: bottom 30%
+2.5,
+3.2,
+4.133333333,
+5.266666667,
+6.966666667,	
+8.666666667,
+10.16666667,
+11.56666667,	
+13.56666667,	
+16.73333333,	
+19.3,	
+22.23333333,	
+26.5,
+31.36666667,	
+37.6
 '''
+
 import pandas as pd
 df2 = pd.read_csv("/Users/yuchenli/Box Sync/Yuchen_project/"
-                  "Truven_rising_stars/oncology_profile/Output_data/"
+                  "Rising_stars/oncology_profile/Output_data/"
                   "number_of_publication_1_15.csv")
 
 
@@ -41,29 +42,30 @@ def compare_list(listA, listB):
     return result
 
 def benchmarking(df2, age_cutoff):
+    df = df2.copy(deep = True)
     age_cutoff = int(age_cutoff)
-    benchmark = list([2.633333333	
-            ,3.4	
-            ,4.4	
-            ,5.766666667	
-            ,7.5	
-            ,9.333333333	
-            ,10.93333333	
-            ,12.33333333	
-            ,14.33333333	
-            ,17.6	
-            ,20.03333333	
-            ,22.96666667	
-            ,26.96666667	
-            ,31.53333333	
-            ,37.6])
+    benchmark = list([2.5,
+                        3.2,
+                        4.133333333,
+                        5.266666667,
+                        6.966666667,	
+                        8.666666667,
+                        10.16666667,
+                        11.56666667,	
+                        13.56666667,	
+                        16.73333333,	
+                        19.3,	
+                        22.23333333,	
+                        26.5,
+                        31.36666667,	
+                        37.6])
     rising_stars = list()
-    for i in range(len(df2)):
-        birth_year = df2.iloc[i,17]
-        KOL = df2.iloc[i,2]
+    for i in range(len(df)):
+        birth_year = df.iloc[i,17]
+        KOL = df.iloc[i,2]
         if pd.isnull(birth_year):
             birth_year = -888888
-        list1 = list(df2.iloc[i,[1,3,4,5,6,7,8,9,10,11,12,13,14,15,16]])
+        list1 = list(df.iloc[i,[1,3,4,5,6,7,8,9,10,11,12,13,14,15,16]])
         
         if KOL == "No":
             if compare_list(list1, benchmark) and (int(birth_year) >= (2017 - age_cutoff)):
@@ -75,16 +77,18 @@ def benchmarking(df2, age_cutoff):
         else:
             rising_stars.append("No")
     
-    df2["Rising_stars"] = rising_stars
-    return df2
+    df["Rising_stars"] = rising_stars
+    return df
 
 df_50 = benchmarking(df2, 50) 
 df_45 = benchmarking(df2, 45)
        
 df_50.to_csv("/Users/yuchenli/Box Sync/Yuchen_project/"
-           "Truven_rising_stars/oncology_profile/Output_data/"
-           "Oncology_profile_rising_stars_age_cutoff_50.csv", index = False) 
+           "Rising_stars/oncology_profile/Output_data/"
+           "Oncology_profile_rising_stars_age_cutoff_50.csv", index = False,
+           na_rep = "NA") 
 
 df_45.to_csv("/Users/yuchenli/Box Sync/Yuchen_project/"
-           "Truven_rising_stars/oncology_profile/Output_data/"
-           "Oncology_profile_rising_stars_age_cutoff_45.csv", index = False) 
+           "Rising_stars/oncology_profile/Output_data/"
+           "Oncology_profile_rising_stars_age_cutoff_45.csv", index = False,
+           na_rep = "NA") 
